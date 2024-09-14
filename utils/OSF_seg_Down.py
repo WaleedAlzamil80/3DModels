37c3271d49b9bc95cc3ba89c532b7c3eead803a5
WaleedAlzamil80 <WaleedAlzamil80@gmail.com>
2024-09-14 17:25:23 +0300

    txt files included also

--- utils/OSF_seg_Down.py
+++ utils/OSF_seg_Down.py
@@ -18,26 +18,24 @@ li = ["xctdy", "5cmg3", "xfcn9", "hw7bj", "n9bd7", "7az58", "2ybr4"]
 
 for i in range(args.n):
     project = osf.project(li[i])  # The project ID from the link
-
+    print(li[i])
     # Access the storage node where the files are located
     storage = project.storage('osfstorage')
 
     # Download each file in the storage
     for file in storage.files:
-        # Ensure we're only handling zip files
-        if file.name.endswith('.zip'):
-            print(f'Downloading {file.name}')
-            # Download the zip file
-            with open(file.name, 'wb') as local_file:
-                file.write_to(local_file)
+        print(f'Downloading {file.name}')
+        # Download the zip file
+        with open(file.name, 'wb') as local_file:
+            file.write_to(local_file)
+        
+        if li[i] != "xctdy":
+            # Extract the zip file
+            with zipfile.ZipFile(file.name, 'r') as zip_ref:
+                extract_path = os.path.splitext(file.name)[0]  # Directory name without .zip
+                print(f'Extracting {file.name} to {extract_path}')
+                zip_ref.extractall(extract_path)
             
-            if li[i] != "xctdy":
-                # Extract the zip file
-                with zipfile.ZipFile(file.name, 'r') as zip_ref:
-                    extract_path = os.path.splitext(file.name)[0]  # Directory name without .zip
-                    print(f'Extracting {file.name} to {extract_path}')
-                    zip_ref.extractall(extract_path)
-                
-                # Optionally, delete the zip file after extraction
-                os.remove(file.name)
-                print(f'{file.name} has been extracted and deleted.')
+            # Optionally, delete the zip file after extraction
+            os.remove(file.name)
+            print(f'{file.name} has been extracted and deleted.')
