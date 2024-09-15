1d93fcc51bb10b2b8c97e187c5fd75f4f76745de
WaleedAlzamil80 <WaleedAlzamil80@gmail.com>
2024-09-15 17:36:57 +0300

    add data loader

---
+++ Dataset/segmentation_OSF/Dataset.py
@@ -0,0 +1,84 @@
+from torch.utils.data import Dataset, DataLoader
+import os
+import json
+import numpy as np
+import trimesh
+
+class TeethSegmentationDataset(Dataset):
+    def __init__(self, root_dir, split='train', test_ids_file='private-testing-set.txt', transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the parts (data_part_{1-6}).
+            split (string): 'train' or 'test' to select the appropriate dataset.
+            test_ids_file (string): Path to the txt file containing IDs for testing.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.root_dir = root_dir
+        self.split = split
+        self.transform = transform
+        self.test_ids = self._load_test_ids(test_ids_file)
+        self.data_list = self._prepare_data_list()
+
+    def _load_test_ids(self, test_ids_file):
+        """Load IDs from private-testing-set.txt."""
+        with open(test_ids_file, 'r') as f:
+            ids = [line.strip() for line in f.readlines()]
+        return ids
+
+    def _prepare_data_list(self):
+        """Prepare the list of data paths for training or testing."""
+        data_list = []
+        for part in range(1, 7):
+            part_dir = os.path.join(self.root_dir, f'data_part_{part}')
+            for region in ['lower', 'upper']:
+                region_dir = os.path.join(part_dir, region)
+                for sample_id in os.listdir(region_dir):
+                    sr = sample_id + "_" + region
+                    if (self.split == 'test' and sr in self.test_ids) or \
+                       (self.split == 'train' and sr not in self.test_ids):
+                        obj_path = os.path.join(region_dir, sample_id, f'{sample_id}_{region}.obj')
+                        label_path = os.path.join(region_dir, sample_id, f'{sample_id}_{region}.json')
+                        data_list.append((obj_path, label_path))
+        return data_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def _load_obj_file(self, obj_path):
+        """Load .obj file and return vertices and faces."""
+        mesh_data = trimesh.load(obj_path)
+        vertices = mesh_data.vertices
+        # faces = mesh_data.faces
+        return vertices
+
+    def _load_labels(self, label_path):
+        """Load labels from the JSON file."""
+        with open(label_path, 'r') as f:
+            labels = json.load(f)
+        return np.array(labels['labels'])
+
+    def __getitem__(self, idx):
+        obj_path, label_path = self.data_list[idx]
+
+        # Load data
+        vertices = self._load_obj_file(obj_path)
+        labels = self._load_labels(label_path)
+
+        # sample = {'vertices': vertices, 'labels': labels}
+
+        # if self.transform:
+        #    sample = self.transform(sample)
+
+        return vertices, labels
+
+# Usage of the dataset
+def get_data_loaders(root_dir, batch_size=4, test_ids_file='private-testing-set.txt'):
+    # Create training and testing datasets
+    train_dataset = TeethSegmentationDataset(root_dir=root_dir, split='train', test_ids_file=test_ids_file)
+    test_dataset = TeethSegmentationDataset(root_dir=root_dir, split='test', test_ids_file=test_ids_file)
+
+    # Create DataLoader for both
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
--- utils/OSF_seg_Down.py
+++ utils/OSF_seg_Down.py
@@ -6,6 +6,8 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Model training parameters")
     parser.add_argument('--n', type=int, default=4, help="Number of Parts to Download")
+    parser.add_argument('--path', type=str, default="dataset", help="Extract to")
+
     return parser.parse_args()
 
 args = parse_args()
@@ -17,25 +19,27 @@ osf = OSF()
 li = ["xctdy", "5cmg3", "xfcn9", "hw7bj", "n9bd7", "7az58", "2ybr4"]
 
 for i in range(args.n):
-    project = osf.project(li[i])  # The project ID from the link
-    print(li[i])
+    project = osf.project(li[i])
+
     # Access the storage node where the files are located
     storage = project.storage('osfstorage')
 
     # Download each file in the storage
     for file in storage.files:
         print(f'Downloading {file.name}')
+
         # Download the zip file
         with open(file.name, 'wb') as local_file:
             file.write_to(local_file)
-        
+
         if li[i] != "xctdy":
             # Extract the zip file
             with zipfile.ZipFile(file.name, 'r') as zip_ref:
-                extract_path = os.path.splitext(file.name)[0]  # Directory name without .zip
+                extract_path = os.path.splitext(file.name)[0]
+                extract_path = os.path.join(args.path, extract_path)
                 print(f'Extracting {file.name} to {extract_path}')
                 zip_ref.extractall(extract_path)
-            
-            # Optionally, delete the zip file after extraction
+
+            # Delete the zip file after extraction
             os.remove(file.name)
             print(f'{file.name} has been extracted and deleted.')
